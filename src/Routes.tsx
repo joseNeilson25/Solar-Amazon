9d18e71b4c66e07db3d997a25522be9fc7d7b160
neilson <joseneilson25@gmail.com>
2022-08-21 23:48:41 -0300

    DeafaultLayout

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { CompleteOrderPage } from "./pages/CompleteOrder";
 import { HomePage } from "./pages/Home";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
     return (
-      <Routes>
+        <Routes>
+        <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/completeOrder" element={<CompleteOrderPage />} />
+        </Route>
       </Routes>
     );
   }
