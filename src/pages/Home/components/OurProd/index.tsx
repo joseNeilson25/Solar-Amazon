8a9a02c84ce27f317e69e335d52369d029a09780
neilson <joseneilson25@gmail.com>
2022-08-24 17:20:02 -0300

    Typography

---
+++ src/pages/Home/components/OurProd/index.tsx
@@ -0,0 +1,10 @@
+import { TitleText } from "../../../../components/Typography";
+import { OurProdContainer } from "./styles";
+
+export function OurProd(){
+    return(
+        <OurProdContainer>
+            <TitleText></TitleText>
+        </OurProdContainer>
+    )
+}
