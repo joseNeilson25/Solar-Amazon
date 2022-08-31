d16d8e5020589f8a9603b1905d5220c60bce5e9e
neilson <joseneilson25@gmail.com>
2022-08-31 13:16:37 -0300

    select em ourProd

--- src/pages/Home/components/OurProd/index.tsx
+++ src/pages/Home/components/OurProd/index.tsx
@@ -2,17 +2,27 @@ import { TitleText } from "../../../../components/Typography";
 import { ProdCard } from "../ProdCard";
 import { OurProdContainer, ProdList } from "./styles";
 import { coffees } from "../../../../data/coffees";
+import { useState } from "react";
 
 export function OurProd(){
+    const [busca, setBusca] = useState('')
+    const coffeesFiltrados = coffees.filter((coffees) => coffees.tags.includes(busca))
+
     return(
         <OurProdContainer className="container">
             <TitleText size="l" color="subtitle">
                 Nossos produtos
             </TitleText>
+            <select onChange={(ev) => setBusca(ev.target.value)}>
+                    <option value="">Todos</option>
+                    <option value="Tradicional ">Tradicional</option>
+                    <option value="com leite">com leite</option>
+                    <option value="Especial ">Especial</option>
+                </select>
             <ProdList>
-                    {coffees.map((coffee) => (
-                    <ProdCard key={coffee.id} coffee={coffee} />
-                ))}
+                            {coffeesFiltrados.map((coffee) => (
+                            <ProdCard key={coffee.id} coffee={coffee} />
+                        ))} 
             </ProdList>
         </OurProdContainer>
     )
